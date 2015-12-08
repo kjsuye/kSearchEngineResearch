45215f945d911a3fee8f6bb82490155f1789630d
Kelsey Suyehira <k.suyehira@gmail.com>
2015-12-08 00:23:34 -0700

    adding a few bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  get  'help' => 'static_pages#help'
+#  get  'help' => 'static_pages#help'
   get  'about' => 'static_pages#about'
 
   get    'admin_login'   => 'admin_sessions#new'
